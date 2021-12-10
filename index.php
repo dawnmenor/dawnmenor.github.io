7b645fba38a82bad12befc1bc10b0be00c402512
Dawn Bernadette Menor <dawnmenor@gmail.com>
2021-12-10 23:58:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html>
+<body>
 
-echo "test";
+<h1 style="background-color: red;">Hello World!</h1>
+<p>This is a paragraph.</p>
+<img src="https://picsum.photos/200" alt="Image 1" width="300" height="300">
+
+<a href="https://www.google.com">This is a link</a>
+
+
+
+<br>
+<p title="I'm a tooltip">This is a paragraph.</p>
+
+<br>
+<p style="color:red;">This is a red paragraph.</p>
+
+
+
+
+</body>
+</html>
